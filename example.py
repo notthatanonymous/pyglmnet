074037d462c43758213f1125af1ad38d56385a72
Ahmad Abdellatif <124534399+notthatanonymous@users.noreply.github.com>
2023-10-17 21:26:34 -0400

    Update example.py

--- example.py
+++ example.py
@@ -23,7 +23,7 @@ missing values.
 ########################################################
 # Imports
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from pyglmnet import GLM, GLMCV, datasets
 
