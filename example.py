8d692713fe6a20e065f02e7eec82714afeae3bc1
Ahmad Abdellatif <124534399+notthatanonymous@users.noreply.github.com>
2023-10-17 21:23:33 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,13 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+======================
+Community and Crime
+======================
+
+This is a real dataset of per capita violent crime, with demographic
+data comprising 128 attributes from 1994 counties in the US.
+
+The original dataset can be found here:
+http://archive.ics.uci.edu/ml/datasets/Communities+and+Crime
+
+The target variables (per capita violent crime) are normalized to lie in
+a [0, 1] range. We preprocessed this dataset to exclude attributes with
+missing values.
+"""
+
+########################################################
+
+# Author: Vinicius Marques <vini.type@gmail.com>
+# License: MIT
+
+########################################################
+# Imports
+
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from pyglmnet import GLM, GLMCV, datasets
-import numpy as np # noqa
-from sklearn.model_selection import GridSearchCV # noqa
-from sklearn.model_selection import KFold # noqa
+
+########################################################
+# Download and preprocess data files
 
 X, y = datasets.fetch_community_crime_data()
 n_samples, n_features = X.shape
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
+########################################################
+# Split the data into training and test sets
+
+X_train, X_test, y_train, y_test = \
+    train_test_split(X, y, test_size=0.33, random_state=0)
+
+########################################################
+# Fit a binomial distributed GLM with elastic net regularization
+
+# use the default value for reg_lambda
+glm = GLMCV(distr='binomial', alpha=0.05, score_metric='pseudo_R2', cv=3,
+            tol=1e-4)
+
+# fit model
+glm.fit(X_train, y_train)
+
+# score the test set prediction
+y_test_hat = glm.predict_proba(X_test)
+print("test set pseudo $R^2$ = %f" % glm.score(X_test, y_test))
+
+########################################################
+# Now use GridSearchCV to compare
+
+import numpy as np # noqa
+from sklearn.model_selection import GridSearchCV # noqa
+from sklearn.model_selection import KFold # noqa
 
 cv = KFold(3)
 
